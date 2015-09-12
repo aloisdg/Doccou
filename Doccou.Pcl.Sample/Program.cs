10f65b1834f8645c5bcf07da6ddd7a8e25b182da
aloisdg <aloisdegouvello@live.fr>
2015-09-12 20:19:58 +0530

    Feat: Write a sample for Doccou.Pcl. Close #20

---
+++ Doccou.Pcl.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Doccou.Pcl.Sample
+{
+	class Program
+	{
+		static void Main()
+		{
+			const string path = "../../Example/example.pdf";
+
+			var document = new Document(path, File.Open(path, FileMode.Open));
+
+			Console.WriteLine("{0} contains {1} pages.",
+				document.Name,
+				document.Count);
+		}
+	}
+}

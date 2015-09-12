1e4baaec31c5d93adb982f29d5c44590046a6819
aloisdg <aloisdegouvello@live.fr>
2015-09-12 20:07:53 +0530

    Feat: Init test solution #19

---
+++ Doccou.Pcl.Test/UnitTestPDF.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Doccou.Pcl.Test
+{
+	[TestFixture]
+	public class UnitTestPdf
+	{
+		[Test]
+		public void TestEmptyPdf()
+		{
+		}
+
+		[Test]
+		public void TestOnePagePdf()
+		{
+		}
+
+		[Test]
+		public void TestBigPdf()
+		{
+		}
+	}
+}
